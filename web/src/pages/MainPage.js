1ed6eafecf1832ade74950c135f7f3720c8c9080
DGGomez <dggomez21@gmail.com>
2019-03-17 09:31:17 -0400

    mainpage

---
+++ web/src/pages/MainPage.js
@@ -0,0 +1,48 @@
+import React, {Component} from "react";
+import {Button, FormGroup, FormControl, ListGroup, ListGroupItem } from "react-bootstrap";
+import { connect } from 'react-redux';
+
+import { readData } from '../actions';
+import Card from '../components/Card';
+import { isNumber } from "util";
+
+class DashboardPage extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: false,
+      yourLocation: "",
+      page: 1
+    };
+  }
+  myClick(){
+    const { history } = this.props;
+    this.props.history.push("/view")
+  }
+  myClick2(){
+    const { history } = this.props;
+    this.props.history.push("/order")
+  }
+  myClick3(){
+    const { history } = this.props;
+    this.props.history.push("/tips")
+  }
+
+  render() {
+    const { report } = this.props;
+    return (
+      <div className="dashboard__container">
+        <div className="profile__container">
+          <div className="profile-body__container"><H1>Welcome to Weed Soil(name pending)</H1></div>
+          <div><p>Hey our product increases the strength and potentcy of the weed you grow. If that sounds good to you order some!</p></div>
+          <div><button onClick={this.myClick2}>Order</button></div>
+          <div><p>already ordered?</p><button onClick={this.myClick}>View</button></div>
+          <div><p>Also here are some growing tips =D<button onClick={this.myClick3}>Tips!</button></p></div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default connect(null, { readData })(DashboardPage);
