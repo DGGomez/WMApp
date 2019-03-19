4840d4f6074f0c20b8bbbde9e8cc1278daa70142
Daniel Gomez <dggomez21@gmail.com>
2019-03-19 09:52:16 -0400

    Update PaymentForm.js

--- web/src/pages/PaymentForm.js
+++ web/src/pages/PaymentForm.js
@@ -2,24 +2,34 @@ import React, {Component} from 'react';
 import {CardElement, injectStripe} from 'react-stripe-elements';
 
 class PaymentForm extends Component {
-  constructor(props) {
-    super(props);
-    this.submit = this.submit.bind(this);
-  }
+constructor(props) {
+  super(props);
+  this.state = {complete: false};
+  this.submit = this.submit.bind(this);
+}
 
-  async submit(ev) {
-    // User clicked submit
-  }
+async submit(ev) {
+  let {token} = await this.props.stripe.createToken({name: "Name"});
+  let response = await fetch("/charge", {
+    method: "POST",
+    headers: {"Content-Type": "text/plain"},
+    body: token.id
+  });
 
-  render() {
-    return (
-      <div className="checkout">
-        <p>Would you like to complete the purchase?</p>
-        <CardElement />
-        <button onClick={this.submit}>Send</button>
-      </div>
-    );
-  }
+  if (response.ok) this.setState({complete: true});
+}
+
+render() {
+  if (this.state.complete) return <h1>Purchase Complete</h1>;
+
+  return (
+    <div className="checkout">
+      <p>Would you like to complete the purchase?</p>
+      <CardElement />
+      <button onClick={this.submit}>Send</button>
+    </div>
+  );
+}
 }
 
 export default injectStripe(PaymentForm);
