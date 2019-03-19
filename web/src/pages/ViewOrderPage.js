cd7ca706bb6cb1844fa7c54cc0f68483a9997099
Daniel Gomez <dggomez21@gmail.com>
2019-03-19 09:42:07 -0400

    Update ViewOrderPage.js

--- web/src/pages/ViewOrderPage.js
+++ web/src/pages/ViewOrderPage.js
@@ -2,41 +2,145 @@ import React, {Component} from "react";
 import {Button, FormGroup, FormControl, ListGroup, ListGroupItem } from "react-bootstrap";
 import { connect } from 'react-redux';
 
+import { readData } from '../actions';
+import Card from '../components/Card';
+import { isNumber } from "util";
+
+const numbers = ['1','2','3','4', '5', '6', '7', '8', '9', '10'];
 
 class DashboardPage extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
+      loading: false,
+      yourLocation: "",
+      page: 1
     };
   }
-  myClick(){
-    const { history } = this.props;
-    this.props.history.push("/view")
+
+  componentDidMount() {
+
+    const { fetchReports, report } = this.props;
+    if(report !== undefined) {
+      this.setState({ loading: true });
+      readData({location: this.state.yourLocation},
+        () => this.setState({ loading: false, page: this.state.page++ })
+      );
+    }
   }
-  myClick2(){
-    const { history } = this.props;
-    this.props.history.push("/order")
-  }
-  myClick3(){
-    const { history } = this.props;
-    this.props.history.push("/tips")
+  componentWillReceiveProps(nextProps) {
+    console.log('reveive props: ', nextProps);
+    const { readData, report } = nextProps;
+    if(report === undefined) {
+      this.setState({ loading: true });
+      readData({ location: this.state.yourLocation },
+        () => this.setState({ loading: false })
+      );
+    }
   }
 
+  renderReport = () => {
+    const { report } = this.props;
+    const { loading } = this.state;
+    if(loading) {
+      return (
+        <div>
+          ...loading
+        </div>
+      );
+    } else {
+      if(report !== undefined) {
+        return (
+            <div className="container">
+              <div className="heading__container">
+                Reports
+              </div>
+              <div className="body__container">
+                {numbers.map((digit,index) => {
+                  return (
+                    <Card title={report[digit].title} location={report[digit].location} description={report[digit].description} />
+                  );
+                  })}
+              </div>
+          </div>
+        );
+     } else {
+       return (
+         <div>
+           No Reports :D
+         </div>
+       );
+     }
+    }
+  }
+  
+  validateForm() {
+    return this.state.yourLocation.length > 1;
+  }
+  
+  onSubmit = e => {
+    e.preventDefault();
+    const { yourLocation } = this.state;
+    this.setState({ loading: true });
+    this.setState({ loading: false });
+  };
+  
+  handleChange = event => {
+    this.setState({
+      [event.target.id]: event.target.value
+    });
+  }
+
+  renderForm = () => {
+    const { loading } = this.state;
+    if(loading) {
+      return (
+        <div>
+          ...loading
+        </div>
+      );
+    } else {
+      return (
+        <div className="preference__container">
+          <div className="preference-heading__container">
+            Location
+          </div>
+          <div className="preference-body__container">
+            <form onSubmit={this.onSubmit}>
+                <FormGroup controlId="yourLocation" bsSize="large">
+                  <div>What location do you want to see?</div>
+                  <FormControl
+                    value={this.state.yourLocation}
+                    onChange={this.handleChange}
+                    type="string"
+                  />
+                </FormGroup>
+                <Button
+                  block
+                  bsSize="large"
+                  type="submit"
+                  disabled={!this.validateForm()}
+                >
+                  Submit
+                </Button>
+              </form>
+          </div>
+        </div>
+      );
+    }
+  }
   render() {
     const { report } = this.props;
     return (
       <div className="dashboard__container">
         <div className="profile__container">
-          <div className="profile-body__container"><H1>Welcome to Weed Soil(name pending)</H1></div>
-          <div><p>Hey our product increases the strength and potentcy of the weed you grow. If that sounds good to you order some!</p></div>
-          <div><button onClick={this.myClick2}>Order</button></div>
-          <div><p>already ordered?</p><button onClick={this.myClick}>View</button></div>
-          <div><p>Also here are some growing tips =D<button onClick={this.myClick3}>Tips!</button></p></div>
+          <div className="profile-body__container">div content</div>
         </div>
+        {report !== undefined ? this.renderReport() : this.renderForm() }
       </div>
     );
   }
 }
 
-export default connect(null)(DashboardPage);
+export default connect(null, { readData })(DashboardPage);
