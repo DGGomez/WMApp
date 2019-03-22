9d6e98d7145ca3313c67ef7cf31d1b28acca479e
Daniel Gomez <dggomez21@gmail.com>
2019-03-22 09:25:30 -0400

    Update CreateUserScreen.js

--- mobile/screens/CreateUserScreen.js
+++ mobile/screens/CreateUserScreen.js
@@ -1,1 +1,15 @@
 //nothing yet
+class CreateUserScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Create',
+  };
+  render() {
+    const {navigate} = this.props.navigation;
+    return (
+      <Button
+        title="Submit"
+        onPress={() => navigate('Home')}
+      />
+    );
+  }
+}
