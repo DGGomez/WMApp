bd609151a3b7a867f9fdcd0502ba9221af6bc3d8
Daniel Gomez <dggomez21@gmail.com>
2019-03-22 09:27:25 -0400

    Update LoginScreen.js

--- mobile/screens/LoginScreen.js
+++ mobile/screens/LoginScreen.js
@@ -1,1 +1,15 @@
 //nothing yet
+class LoginScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Login',
+  };
+  render() {
+    const {navigate} = this.props.navigation;
+    return (
+      <Button
+        title="Login"
+        onPress={() => navigate('Home')}
+      />
+    );
+  }
+}
