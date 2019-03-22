39b5136d1bc416910fccf4d75b296d246c5715e9
Daniel Gomez <dggomez21@gmail.com>
2019-03-22 09:30:57 -0400

    Update ViewOrderScreen.js

--- mobile/screens/ViewOrderScreen.js
+++ mobile/screens/ViewOrderScreen.js
@@ -1,1 +1,11 @@
 //nothing yet
+class ViewOrderScreen extends React.Component {
+  static navigationOptions = {
+    title: 'ViewOrder',
+  };
+  render() {
+    const {navigate} = this.props.navigation;
+    return (
+    );
+  }
+}
