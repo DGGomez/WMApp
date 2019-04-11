6be0edddc4f2121ee1ecee5513f1ceae05fd471b
Daniel Gomez <dggomez21@gmail.com>
2019-04-11 14:31:44 -0400

    Update index.js

--- web/src/reducers/index.js
+++ web/src/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
+import orderReducer from './orderReducer';
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+   order: orderReducer
 });
 
 export default rootReducer;
