174c1a91cc9362d9b65826b00adcf472e95bfead
Daniel Gomez <dggomez21@gmail.com>
2019-04-15 09:26:27 -0400

    Update dataActions.js

--- mobile/actions/dataActions.js
+++ mobile/actions/dataActions.js
@@ -48,8 +48,75 @@ class UserApi {
 
   async login(args) {
     try {
-      const { data } = await axios.post(`${this.path}/auth0`, args);
+      const { data } = await axios.post(`${this.path}/login`, args);
       return data;
+    } catch (e) {
+      throw e;
+    }
+  }
+  
+    async RegisterUser(args) {
+    try {
+      const { data } = await axios.post(`${this.path}/register`, args);
+      return data;
+    } catch (e) {
+      throw e;
+    }
+   
+    async LogoutUser() {
+    try {
+      await removeAuthToken();
+      dispatch({
+            type: 'LOGOUT_USER'
+        });
+    } catch (e) {
+      throw e;
+    }
+      
+    async storeAuthToken() {
+    try {
+     await localStorage.setItem('authToken', JSON.stringify(token));
+    } catch (e) {
+      throw e;
+    }   
+     
+    async fetchAuthToken() {
+    try {
+     let token = await localStorage.getItem('authToken');
+      return JSON.parse(token);
+    } catch (e) {
+      throw e;
+    }   
+      
+    async removeAuthToken() {
+    try {
+      await localStorage.removeItem('authToken');
+    } catch (e) {
+      throw e;
+    }   
+            
+    async tokenLogin() {
+    try {
+      let token = await fetchAuthToken();
+      if(token) {
+      const { data } = await axios({
+                method: 'get',
+                url: `${this.path}/token-login`,
+                headers: { 'Authorization': `Bearer ${token}` }
+            });
+      
+            storeAuthToken(data.token);
+            dispatch({
+                type: 'TOKEN_LOGIN',
+                payload: {
+                    user: data.user,
+                    token: data.token
+                }
+            });
+        
+        } else {
+            console.log('no token');
+        }
     } catch (e) {
       throw e;
     }
