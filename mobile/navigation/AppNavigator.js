d8d2bc519c42e856354a6c355e1ede40d0f3c98e
Daniel Gomez <dggomez21@gmail.com>
2019-04-16 11:52:05 -0400

    Update AppNavigator.js

--- mobile/navigation/AppNavigator.js
+++ mobile/navigation/AppNavigator.js
@@ -1,10 +1,124 @@
 import React from 'react';
-import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createAppContainer, createStackNavigator, createBottomTabNavigator, createSwitchNavigator } from 'react-navigation';
 
 import MainTabNavigator from './MainTabNavigator';
 
-export default createAppContainer(createSwitchNavigator({
-  // You could add another route here for authentication.
-  // Read more at https://reactnavigation.org/docs/en/auth-flow.html
-  Main: MainTabNavigator,
-}));
+const primaryHeader = {
+  headerStyle: {
+    backgroundColor: theme.color.green,
+  },
+  headerTintColor: theme.color.white,
+  headerTitleStyle: {
+    fontWeight: '400',
+  },
+};
+
+const AuthNavigator = createStackNavigator(
+  {
+    Login: {
+      getScreen: () => require('./LoginScreen').default,
+    },
+  },
+  {
+    navigationOptions: {
+      header: null,
+    },
+  },
+);
+
+const ShoppingCartNavigator = createStackNavigator({
+  ShoppingCart: {
+    getScreen: () => require('./ViewOrderScreen').default,
+    navigationOptions: {
+      headerStyle: {
+        backgroundColor: theme.color.white,
+      },
+    },
+  },
+});
+
+const HomeStack = createStackNavigator(
+  {
+    Home: {
+      getScreen: () => require('./HomeScreen').default,
+    },
+    Category: {
+      getScreen: () => require('./SettingScreen').default,
+    },
+    ShoppingCart: {
+      screen: ShoppingCartNavigator,
+      navigationOptions: {
+        header: null,
+      },
+    },
+  },
+  {
+    navigationOptions: { ...primaryHeader, headerRight: <ShoppingCartIcon /> },
+  },
+);
+
+HomeStack.navigationOptions = ({ navigation }) => {
+  let tabBarVisible = true;
+
+  console.log('navigation', navigation);
+
+  if (
+    NavigationService.getCurrentRouteName(navigation.state) === 'ShoppingCart'
+  ) {
+    tabBarVisible = false;
+  }
+
+  return {
+    tabBarVisible,
+  };
+};    
+const TabNavigator = createBottomTabNavigator(
+  {
+    Home: HomeStack,
+    Order: {
+      getScreen: () => require('./OrderScreen').default,
+    },
+    OrderView: {
+      getScreen: () => require('./ViewOrderScreen').default,
+    },
+  },
+  {
+    tabBarComponent: props => <TabBar {...props} />,
+  },
+);
+
+const MainNavigator = createStackNavigator(
+  {
+    Tab: TabNavigator
+  },
+  {
+    mode: 'modal',
+    navigationOptions: {
+      header: null,
+    },
+  },
+);
+
+const AppNavigator = createSwitchNavigator(
+  {
+//     Splash: {
+//       getScreen: () => require('./SplashScreen').default,
+//     },
+    Auth: AuthNavigator,
+    Main: MainNavigator,
+  },
+  {
+    initialRouteName: 'Main',
+  },
+);
+
+class Navigation extends Component {
+  state = {};
+  render() {
+    return (
+      <AppNavigator ref={r => NavigationService.setTopLevelNavigator(r)} />
+    );
+  }
+}
+
+export default Navigation;
